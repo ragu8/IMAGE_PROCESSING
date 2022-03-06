a1d4526c9651edac8880497754b8d3b60263c478
RAGUPATHI-M <ragumdr812@gmail.com>
2022-03-06 20:55:13 +0530

    image processing

--- Image Processing.py
+++ Image Processing.py
@@ -31,19 +31,35 @@ img_copy = img.copy()
 
 # Image overlap
 img1 = Image.open("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/myfriends.jpg")
-img2 = Image.open("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/myfriends.jpg"))
+img2 = Image.open("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/friends.png")
+resize_img2 = img2.resize((1000,750))
 img_overlap = img1.copy()
-img_overlap.paste(img2,(50,50))
+img_overlap.paste(resize_img2,(1000,1000))
 img_overlap.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/overlap.jpg")
 
+# Image rotation
+img_90 = img.rotate(90)
+img_90.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/rotation_90.jpg") 
 
+img_45 = img.rotate(45)
+img_45.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/rotation_45.jpg")
 
+img_180 = img.rotate(180)
+img_180.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/rotation_180.jpg")
 
+img_8 = img.rotate(8)
+img_8.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/rotation_8.jpg")
 
+# Flip
+img_Left_to_Right = img.transpose(Image.FLIP_LEFT_RIGHT)
+img_Left_to_Right.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/flip_left_to_right.jpg")
 
+img_top_to_buttom = img.transpose(Image.FLIP_TOP_BOTTOM)
+img_top_to_buttom.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/flip_top_to_buttom.jpg")
 
-
-
+# Grayscale
+grey_img = img.convert("L")
+grey_img.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/grey.jpg")
 
 
 
