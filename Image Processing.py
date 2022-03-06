e5ad7862a588aec76c933f1ad591b38ded0a0a96
RAGUPATHI-M <ragumdr812@gmail.com>
2022-03-06 21:03:15 +0530

    image processing

--- Image Processing.py
+++ Image Processing.py
@@ -1,7 +1,8 @@
+# import module
 from PIL import Image
 
 # Loading the image
-img = Image.open("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/myfriends.jpg")
+img = Image.open("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/myfriends.jpg")
 
 # Understanding about image
 print(img.format)
@@ -13,55 +14,56 @@ resize_img = img.resize((1000,1500))
 print(resize_img.size)
 
 # Saving the resize image
-resize_img.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/resize.jpg")
+resize_img.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/resize.jpg")
 
 # Thumbnail
 img.thumbnail((650,600))
-img.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/thumbnail.jpg")
+img.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/thumbnail.jpg")
 print(img.size)
 
 # Crop
-img = Image.open("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/myfriends.jpg")
+img = Image.open("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/myfriends.jpg")
 cropped_img = img.crop((0,0,300,300)) 
-img.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/cropping.jpg")
+img.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/cropping.jpg")
 print(cropped_img.size)
 
 # copy
 img_copy = img.copy()
 
 # Image overlap
-img1 = Image.open("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/myfriends.jpg")
-img2 = Image.open("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/friends.png")
+img1 = Image.open("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/myfriends.jpg")
+img2 = Image.open("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/friends.png")
 resize_img2 = img2.resize((1000,750))
 img_overlap = img1.copy()
 img_overlap.paste(resize_img2,(1000,1000))
-img_overlap.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/overlap.jpg")
+img_overlap.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/overlap.jpg")
 
 # Image rotation
 img_90 = img.rotate(90)
-img_90.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/rotation_90.jpg") 
+img_90.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/rotation_90.jpg") 
 
 img_45 = img.rotate(45)
-img_45.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/rotation_45.jpg")
+img_45.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/rotation_45.jpg")
 
 img_180 = img.rotate(180)
-img_180.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/rotation_180.jpg")
+img_180.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/rotation_180.jpg")
 
 img_8 = img.rotate(8)
-img_8.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/rotation_8.jpg")
+img_8.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/rotation_8.jpg")
 
 # Flip
 img_Left_to_Right = img.transpose(Image.FLIP_LEFT_RIGHT)
-img_Left_to_Right.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/flip_left_to_right.jpg")
+img_Left_to_Right.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/flip_left_to_right.jpg")
 
 img_top_to_buttom = img.transpose(Image.FLIP_TOP_BOTTOM)
-img_top_to_buttom.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/flip_top_to_buttom.jpg")
+img_top_to_buttom.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/flip_top_to_buttom.jpg")
 
 # Grayscale
 grey_img = img.convert("L")
-grey_img.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/grey.jpg")
+grey_img.save("Z:/GITHUB_REPOSITORIES/IMAGE_PROCESSING/image_samples/grey.jpg")
 
 
+ 
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# IMAGE_PROCESSING
-Image processing is a method to perform some operations on an image, in order to get an enhanced image or to extract some useful information from it. 
+# IMAGE PROCESSING
+## C o n t e n t s
+
+import module
+
+Loading the image
+
+Resize
+
+Saving the resize image
+
+Thumbnail
+
+Crop
+
+Image overlap
+
+Image rotation
+
+Flip
+
+Grayscale
